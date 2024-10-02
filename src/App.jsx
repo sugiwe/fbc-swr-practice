9757d0bc63e09340935e50149ea42f74d30e43a7
hiromisugie <lozz.gggg@gmail.com>
2024-10-02 14:58:05 +0900

    SWRの導入

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,20 @@
-import { useState } from "react";
+import useSWR from "swr";
 import "./App.css";
+
+const fetcher = (url) =>
+  fetch(url, { headers: { Accept: "application/json" } }).then((res) =>
+    res.json()
+  );
 
 function App() {
   const url = "https://httpstat.us/200?sleep=2000";
-  const headers = { Accept: "application/json" };
 
-  const [status, setStatus] = useState("");
+  const { data, error, isLoading } = useSWR(url, fetcher);
 
-  fetch(url, { headers })
-    .then((res) => res.json())
-    .then((json) => setStatus(json.description));
+  if (error) return <p>Failed to load.</p>;
+  if (isLoading) return <p>Loading...</p>;
 
-  return <>{status && <p>Status : {status}</p>}</>;
+  return <>{data && <p>Status : {data.description}</p>}</>;
 }
 
 export default App;
